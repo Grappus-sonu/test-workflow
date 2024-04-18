3d4fae3ac8d5634f9fdafe7c185c6880fb1c73c1
Grappus-sonu <121923683+Grappus-sonu@users.noreply.github.com>
2024-04-18 11:10:05 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd /var/opt/uat-protean/web-sdk
-sudo awk '/^[[:space:]]+image/ && !p_chng{sub(/:.+$/,": $image_path"); p_chng=1} 1' websdk-deployment.yml
+sudo awk '/^[[:space:]]+image/ && !p_chng{sub(/:.+$/,": $1"); p_chng=1} 1' websdk-deployment.yml
 cat websdk-deployment.yml
